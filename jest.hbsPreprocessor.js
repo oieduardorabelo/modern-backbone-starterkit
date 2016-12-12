38f81e57e56e303b45b634f1d8216ef71eb4f5f3
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-12-13 02:23:46 +1300

    more comments

--- jest.hbsPreprocessor.js
+++ jest.hbsPreprocessor.js
@@ -4,6 +4,10 @@ const hb = require('handlebars')
 
 module.exports = {
   process(src, filename) {
+    //
+    // needs to return the correct template object to the Marionette View,
+    // currently, we are compiling hbs without data
+    //
     return `
       module.exports = ${JSON.stringify(hb.compile(src)({}))}
     `;
